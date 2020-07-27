7101319e937cc486b6537bbd1f74dfcfb077ebd5
Evgeny Zuev <zueven@mts.by>
2020-07-27 17:51:22 +0300

    Add two methods in Reservation address method Main - main logic get_free_ip - get free IP from Netbox by random Prefix get_free_ip - get free IP from Netbox by specified Prefix

--- reservation_address.py
+++ reservation_address.py
@@ -15,5 +15,22 @@ Output data: free address - Netbox json-string)
 import requests
 import netbox
 
+
+def get_free_ip():
+    return {}
+
+
+def get_free_ip_by_prefix(prefix):
+    return {}
+
+
+def main():
+    input_data = {"region": "minsk", "type": "mobile", "prefix": ""}
+    if input_data.get("prefix"):
+        free_ip = get_free_ip_by_prefix(input_data.get("prefix"))
+    free_ip = get_free_ip()
+    return free_ip
+
+
 if __name__ == '__main__':
-    pass
+    address = main()
