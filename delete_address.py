01a6c994b0513045ec9b59b3b3982c50a0deea1c
Evgeny Zuev <zueven@mts.by>
2020-08-04 13:23:15 +0300

    Add logic for delete_address_vrf_global for global VRF

--- delete_address.py
+++ delete_address.py
@@ -46,6 +46,20 @@ def delete_address(address, in_vrf):
         return {"status": "good", "message": f"Address {address} in VRF {in_vrf.get('name')} deleted"}
 
 
+def delete_address_vrf_global(address, region, prefix_type):
+    """Delete ip address from Netbox"""
+    address_params = netbox.Read().Addresses().get_by_address_and_three_tags(address, "erip", region, prefix_type)
+    if address_params.get("count") is None:
+        return {"status": "error", "message": f"Address {address} in GRT don't deleted"}
+    else:
+        try:
+            address_id = address_params.get("results")[0].get("id")
+        except IndexError as index_error:
+            return {"status": "error", "message": index_error}
+        netbox.Delete().Addresses().delete_by_id(address_id)
+        return {"status": "good", "message": f"Address {address} in GRT deleted"}
+
+
 def main():
     """Main logic with filters and checks"""
     # checking parameter passing
@@ -78,18 +92,22 @@ def main():
         return {"status": "error",
                 "message": f"Prefix type required or the entered type is invalid. Enter one of the options: {types}"}
 
-    # checking vrf name type
-    if not isinstance(input_data.get("vrf").get("name"), str):
-        return {"status": "error", "message": "The entered VRF Name is not str type"}
+    region = input_data.get("region")
+    prefix_type = input_data.get("type")
 
-    # checking rd type
-    if not isinstance(input_data.get("vrf").get("rd"), int):
-        return {"status": "error", "message": "The entered RD is not int type"}
-
-    address = input_data.get("address")
-    in_vrf = input_data.get("vrf")
-    message = delete_address(address, in_vrf)
-
+    if input_data.get("vrf") is None:
+        address = input_data.get("address")
+        message = delete_address_vrf_global(address, region, prefix_type)
+    else:
+        # checking vrf name type
+        if not isinstance(input_data.get("vrf").get("name"), str):
+            return {"status": "error", "message": "The entered VRF Name is not str type"}
+        # checking rd type
+        if not isinstance(input_data.get("vrf").get("rd"), int):
+            return {"status": "error", "message": "The entered RD is not int type"}
+        address = input_data.get("address")
+        in_vrf = input_data.get("vrf")
+        message = delete_address(address, in_vrf)
     return message
 
 
